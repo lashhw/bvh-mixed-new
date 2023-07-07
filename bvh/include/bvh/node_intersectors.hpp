f43efa62c56238f43a1bf1fb5fff3d61bccbc59d
Jacky Huang <lashhw81@gmail.com>
2023-07-07 13:58:24 +0800

    revert to original node_intersectors.hpp

--- bvh/include/bvh/node_intersectors.hpp
+++ bvh/include/bvh/node_intersectors.hpp
@@ -27,19 +27,19 @@ struct NodeIntersector {
 
     template <bool IsMin>
     bvh_always_inline
-    Scalar intersect_axis(int axis, Scalar p, const Ray<Scalar>& ray, bool high_precision) const {
-        return static_cast<const Derived*>(this)->template intersect_axis<IsMin>(axis, p, ray, high_precision);
+    Scalar intersect_axis(int axis, Scalar p, const Ray<Scalar>& ray) const {
+        return static_cast<const Derived*>(this)->template intersect_axis<IsMin>(axis, p, ray);
     }
 
     bvh_always_inline
-    std::pair<Scalar, Scalar> intersect(const typename Bvh::Node& node, const Ray<Scalar>& ray, bool high_precision) const {
+    std::pair<Scalar, Scalar> intersect(const typename Bvh::Node& node, const Ray<Scalar>& ray) const {
         Vector3<Scalar> entry, exit;
-        entry[0] = intersect_axis<true >(0, node.bounds[0 * 2 +     octant[0]], ray, high_precision);
-        entry[1] = intersect_axis<true >(1, node.bounds[1 * 2 +     octant[1]], ray, high_precision);
-        entry[2] = intersect_axis<true >(2, node.bounds[2 * 2 +     octant[2]], ray, high_precision);
-        exit [0] = intersect_axis<false>(0, node.bounds[0 * 2 + 1 - octant[0]], ray, high_precision);
-        exit [1] = intersect_axis<false>(1, node.bounds[1 * 2 + 1 - octant[1]], ray, high_precision);
-        exit [2] = intersect_axis<false>(2, node.bounds[2 * 2 + 1 - octant[2]], ray, high_precision);
+        entry[0] = intersect_axis<true >(0, node.bounds[0 * 2 +     octant[0]], ray);
+        entry[1] = intersect_axis<true >(1, node.bounds[1 * 2 +     octant[1]], ray);
+        entry[2] = intersect_axis<true >(2, node.bounds[2 * 2 +     octant[2]], ray);
+        exit [0] = intersect_axis<false>(0, node.bounds[0 * 2 + 1 - octant[0]], ray);
+        exit [1] = intersect_axis<false>(1, node.bounds[1 * 2 + 1 - octant[1]], ray);
+        exit [2] = intersect_axis<false>(2, node.bounds[2 * 2 + 1 - octant[2]], ray);
         // Note: This order for the min/max operations is guaranteed not to produce NaNs
         return std::make_pair(
             robust_max(entry[0], robust_max(entry[1], robust_max(entry[2], ray.tmin))),
@@ -71,7 +71,7 @@ struct RobustNodeIntersector : public NodeIntersector<Bvh, RobustNodeIntersector
 
     template <bool IsMin>
     bvh_always_inline
-    Scalar intersect_axis(int axis, Scalar p, const Ray<Scalar>& ray, bool) const {
+    Scalar intersect_axis(int axis, Scalar p, const Ray<Scalar>& ray) const {
         return (p - ray.origin[axis]) * (IsMin ? inverse_direction[axis] : padded_inverse_direction[axis]);
     }
 
@@ -95,129 +95,12 @@ struct FastNodeIntersector : public NodeIntersector<Bvh, FastNodeIntersector<Bvh
 
     template <bool>
     bvh_always_inline
-    Scalar intersect_axis(int axis, Scalar p, const Ray<Scalar>&, bool) const {
+    Scalar intersect_axis(int axis, Scalar p, const Ray<Scalar>&) const {
         return fast_multiply_add(p, inverse_direction[axis], scaled_origin[axis]);
     }
 
     using NodeIntersector<Bvh, FastNodeIntersector<Bvh>>::intersect;
 };
-
-/// Multiprecision intersection algorithm.
-template <typename Bvh, mpfr_prec_t mantissa_width, mpfr_exp_t exponent_width>
-struct MPNodeIntersector : public NodeIntersector<Bvh, MPNodeIntersector<Bvh, mantissa_width, exponent_width>> {
-    using Scalar = typename Bvh::ScalarType;
-
-    static constexpr mpfr_exp_t exponent_min = -(1 << (exponent_width - 1)) + 2;
-    static constexpr mpfr_exp_t exponent_max = (1 << (exponent_width - 1));
-
-    static bool initialized;
-    static mpfr_t origin_d[3];
-    static mpfr_t origin_u[3];
-    static mpfr_t direction_d[3];
-    static mpfr_t direction_u[3];
-    static mpfr_t tmp;
-
-    Vector3<Scalar> scaled_origin;
-    Vector3<Scalar> inverse_direction;
-
-    void check_exponent_and_set_inf(mpfr_t &num) const {
-        if (mpfr_number_p(num)) {
-            if (mpfr_get_exp(num) < exponent_min) mpfr_set_zero(num, mpfr_signbit(num) ? -1 : 1);
-            else if (mpfr_get_exp(num) > exponent_max) mpfr_set_inf(num, mpfr_signbit(num) ? -1 : 1);
-        }
-    }
-
-    MPNodeIntersector(const Ray<Scalar>& ray)
-            : NodeIntersector<Bvh, MPNodeIntersector<Bvh, mantissa_width, exponent_width>>(ray)
-    {
-        if (!initialized) {
-            for (int i = 0; i < 3; i++) {
-                mpfr_init2(origin_d[i], mantissa_width + 1);
-                mpfr_init2(origin_u[i], mantissa_width + 1);
-                mpfr_init2(direction_d[i], mantissa_width + 1);
-                mpfr_init2(direction_u[i], mantissa_width + 1);
-            }
-
-            mpfr_init2(tmp, mantissa_width + 1);
-
-            initialized = true;
-        }
-
-        inverse_direction = ray.direction.safe_inverse();
-        scaled_origin     = -ray.origin * inverse_direction;
-
-        for (int i = 0; i < 3; i++) {
-            mpfr_set_d(origin_d[i], ray.origin[i], MPFR_RNDD);
-            mpfr_set_d(origin_u[i], ray.origin[i], MPFR_RNDU);
-            mpfr_set_d(direction_d[i], ray.direction[i], MPFR_RNDD);
-            mpfr_set_d(direction_u[i], ray.direction[i], MPFR_RNDU);
-            check_exponent_and_set_inf(origin_d[i]);
-            check_exponent_and_set_inf(origin_u[i]);
-            check_exponent_and_set_inf(direction_d[i]);
-            check_exponent_and_set_inf(direction_u[i]);
-        }
-    }
-
-    template <bool IsMin>
-    bvh_always_inline
-    Scalar intersect_axis(int axis, Scalar p, const Ray<Scalar>& ray, bool high_precision) const {
-        if (high_precision) {
-            return fast_multiply_add(p, inverse_direction[axis], scaled_origin[axis]);
-        }
-        if (!std::signbit(ray.direction[axis])) {
-            if (IsMin) {
-                mpfr_set_d(tmp, p, MPFR_RNDD);
-                check_exponent_and_set_inf(tmp);
-                mpfr_sub(tmp, tmp, origin_u[axis], MPFR_RNDD);
-                check_exponent_and_set_inf(tmp);
-                mpfr_div(tmp, tmp, direction_u[axis], MPFR_RNDD);
-                check_exponent_and_set_inf(tmp);
-            } else {
-                mpfr_set_d(tmp, p, MPFR_RNDU);
-                check_exponent_and_set_inf(tmp);
-                mpfr_sub(tmp, tmp, origin_d[axis], MPFR_RNDU);
-                check_exponent_and_set_inf(tmp);
-                mpfr_div(tmp, tmp, direction_d[axis], MPFR_RNDU);
-                check_exponent_and_set_inf(tmp);
-            }
-        } else {
-            if (IsMin) {
-                mpfr_set_d(tmp, p, MPFR_RNDU);
-                check_exponent_and_set_inf(tmp);
-                mpfr_sub(tmp, tmp, origin_d[axis], MPFR_RNDU);
-                check_exponent_and_set_inf(tmp);
-                mpfr_div(tmp, tmp, direction_d[axis], MPFR_RNDD);
-                check_exponent_and_set_inf(tmp);
-            } else {
-                mpfr_set_d(tmp, p, MPFR_RNDD);
-                check_exponent_and_set_inf(tmp);
-                mpfr_sub(tmp, tmp, origin_u[axis], MPFR_RNDD);
-                check_exponent_and_set_inf(tmp);
-                mpfr_div(tmp, tmp, direction_u[axis], MPFR_RNDU);
-                check_exponent_and_set_inf(tmp);
-            }
-        }
-        return mpfr_get_d(tmp, MPFR_RNDN);
-    }
-};
-
-template <typename Bvh, mpfr_prec_t mantissa_width, mpfr_exp_t exponent_width>
-bool MPNodeIntersector<Bvh, mantissa_width, exponent_width>::initialized = false;
-
-template <typename Bvh, mpfr_prec_t mantissa_width, mpfr_exp_t exponent_width>
-mpfr_t MPNodeIntersector<Bvh, mantissa_width, exponent_width>::origin_d[3];
-
-template <typename Bvh, mpfr_prec_t mantissa_width, mpfr_exp_t exponent_width>
-mpfr_t MPNodeIntersector<Bvh, mantissa_width, exponent_width>::origin_u[3];
-
-template <typename Bvh, mpfr_prec_t mantissa_width, mpfr_exp_t exponent_width>
-mpfr_t MPNodeIntersector<Bvh, mantissa_width, exponent_width>::direction_d[3];
-
-template <typename Bvh, mpfr_prec_t mantissa_width, mpfr_exp_t exponent_width>
-mpfr_t MPNodeIntersector<Bvh, mantissa_width, exponent_width>::direction_u[3];
-
-template <typename Bvh, mpfr_prec_t mantissa_width, mpfr_exp_t exponent_width>
-mpfr_t MPNodeIntersector<Bvh, mantissa_width, exponent_width>::tmp;
 
 } // namespace bvh
 
